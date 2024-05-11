a9690334eaf1a7041196c43e1a20a7e7eff507e6
Hey R You Are <55276676+Abab-bk@users.noreply.github.com>
2024-05-11 23:14:26 +0800

    升级到Godot4.3dev6，优化背景

--- Src/Scripts/GameWorld.cs
+++ Src/Scripts/GameWorld.cs
@@ -12,79 +12,79 @@ namespace NovaDrift.Scripts;
 
 public partial class GameWorld : Node2D
 {
-    private Joystick _moveJoystick;
-    private Joystick _aimJoystick;
-    
-    public override void _Ready()
-    {
-        SoundManager.PlayMusic(GD.Load<AudioStream>(SoundPaths.ZeroGravity));
-        
-        GetNode<Joystick>("%MoveJoystick").Hide();
-        GetNode<Joystick>("%AimJoystick").Hide();
-        
-        EventBus.OnGameInit += Init;
-        EventBus.OnGameOver += GameOver;
-        
-        Global.Init();
-        DataBuilder.Init();
-        AcidSaver.LoadAll();
-        
-        if (Global.CurrentPlatform != GamePlatform.Desktop)
-        {
-            EventBus.OnGameInit?.Invoke();
-            return;
-        }
-        
-        if (OS.GetEnvironment("ShowStartMenu") == "true")
-        {
-            UiManager.Open_StartMenu();
-            return;
-        }
-        
-        EventBus.OnGameInit?.Invoke();
-    }
+	private Joystick _moveJoystick;
+	private Joystick _aimJoystick;
+	
+	public override void _Ready()
+	{
+		SoundManager.PlayMusic(GD.Load<AudioStream>(SoundPaths.ZeroGravity));
+		
+		GetNode<Joystick>("%MoveJoystick").Hide();
+		GetNode<Joystick>("%AimJoystick").Hide();
+		
+		EventBus.OnGameInit += Init;
+		EventBus.OnGameOver += GameOver;
+		
+		Global.Init();
+		DataBuilder.Init();
+		AcidSaver.LoadAll();
+		
+		if (Global.CurrentPlatform != GamePlatform.Desktop)
+		{
+			EventBus.OnGameInit?.Invoke();
+			return;
+		}
+		
+		if (OS.GetEnvironment("ShowStartMenu") == "true")
+		{
+			UiManager.Open_StartMenu();
+			return;
+		}
+		
+		EventBus.OnGameInit?.Invoke();
+	}
 
-    // 顺序是：GameInit => GameStart => GameOver
-    private void Init()
-    {
-        // 更新能力池
-        DataBuilder.BuildAbilityIdPool();
-        
-        _moveJoystick = GetNode<Joystick>("%MoveJoystick");
-        _aimJoystick = GetNode<Joystick>("%AimJoystick");
-        
-        _moveJoystick.Visible = Global.CurrentPlatform != GamePlatform.Desktop;
-        _aimJoystick.Visible = Global.CurrentPlatform != GamePlatform.Desktop;
-        
-        UiManager.Open_Hud();
-        
-        Global.GameWorld = this;
-        
-        Player player = GD.Load<PackedScene>("res://Scenes/Prefabs/Actors/Player.tscn").Instantiate<Player>();
-        player.JoystickNode = _moveJoystick;
-        AddChild(player);
+	// 顺序是：GameInit => GameStart => GameOver
+	private void Init()
+	{
+		// 更新能力池
+		DataBuilder.BuildAbilityIdPool();
+		
+		_moveJoystick = GetNode<Joystick>("%MoveJoystick");
+		_aimJoystick = GetNode<Joystick>("%AimJoystick");
+		
+		_moveJoystick.Visible = Global.CurrentPlatform != GamePlatform.Desktop;
+		_aimJoystick.Visible = Global.CurrentPlatform != GamePlatform.Desktop;
+		
+		UiManager.Open_Hud();
+		
+		Global.GameWorld = this;
+		
+		Player player = GD.Load<PackedScene>("res://Scenes/Prefabs/Actors/Player.tscn").Instantiate<Player>();
+		player.JoystickNode = _moveJoystick;
+		AddChild(player);
 
-        MoveActorToScreenCenter mover = new MoveActorToScreenCenter(player);
-        AddChild(mover);
-        
-        // UiManager.Get_Hud_Instance()[0].OpenNestedUi(UiManager.UiName.StatsMonitor);
-        
-        UiManager.Open_PausedMenu();
-        UiManager.Hide_PausedMenu();
-        
-        EventBus.OnGameStart?.Invoke();
-        
-        Global.SetWorldColor(Constants.Colors.Red);
-    }
+		MoveActorToScreenCenter mover = new MoveActorToScreenCenter(player);
+		AddChild(mover);
+		
+		// UiManager.Get_Hud_Instance()[0].OpenNestedUi(UiManager.UiName.StatsMonitor);
+		
+		UiManager.Open_PausedMenu();
+		UiManager.Hide_PausedMenu();
+		
+		EventBus.OnGameStart?.Invoke();
+		
+		Global.SetWorldColor(Constants.Colors.Red);
+	}
 
-    private async void GameOver()
-    {
-        GetTree().CallGroup("Mobs", Node.MethodName.QueueFree);
-        
-        UiManager.Destroy_Hud();
-        UiManager.Destroy_PausedMenu();
-        
-        Global.Player.CallDeferred(Node.MethodName.QueueFree);
-        UiManager.Open_GameOver();
-    }
+	private async void GameOver()
+	{
+		GetTree().CallGroup("Mobs", Node.MethodName.QueueFree);
+		
+		UiManager.Destroy_Hud();
+		UiManager.Destroy_PausedMenu();
+		
+		Global.Player.CallDeferred(Node.MethodName.QueueFree);
+		UiManager.Open_GameOver();
+	}
 }
--- Src/Scripts/Ui/hud/HudPanel.cs
+++ Src/Scripts/Ui/hud/HudPanel.cs
@@ -10,108 +10,108 @@ namespace NovaDrift.Scripts.Ui.Hud;
 
 public partial class HudPanel : Hud
 {
-    private TextureProgressBar _expBar;
-    private TextureProgressBar _hpBar;
-    private TextureProgressBar _shieldBar;
-    
-    private bool _isDebugPanelOpened = false;
+	private TextureProgressBar _expBar;
+	private TextureProgressBar _hpBar;
+	private TextureProgressBar _shieldBar;
+	
+	private bool _isDebugPanelOpened = false;
 
-    [Export] private Control _debugPanel;
-    
-    public override void OnCreateUi()
-    {
-        _expBar = S_ExpProgressBar.Instance;
-        _hpBar = S_HpProgressBar.Instance;
-        _shieldBar = S_ShieldProgressBar.Instance;
+	[Export] private Control _debugPanel;
+	
+	public override void OnCreateUi()
+	{
+		_expBar = S_ExpProgressBar.Instance;
+		_hpBar = S_HpProgressBar.Instance;
+		_shieldBar = S_ShieldProgressBar.Instance;
 
-        EventBus.OnPlayerUpLevel += i =>
-        {
-            OpenNestedUi(UiManager.UiName.SelectAbility);
-            Global.StopGame();
-        };
-        EventBus.OnGameOver += Destroy;
-        
-        GenerateDebugPanel();
-    }
-    
-    public void UpdateExpBar(float ratio)
-    {
-        _expBar.SetDeferred(Range.PropertyName.Value, ratio * 100f);
-    }
-    
-    public void UpdateHpBar(float ratio)
-    {
-        _hpBar.SetDeferred(Range.PropertyName.Value, ratio * 100f);
-    }
-    
-    public void UpdateShieldBar(float ratio)
-    {
-        _shieldBar.SetDeferred(Range.PropertyName.Value, ratio * 100f);
-    }
+		EventBus.OnPlayerUpLevel += i =>
+		{
+			OpenNestedUi(UiManager.UiName.SelectAbility);
+			Global.StopGame();
+		};
+		EventBus.OnGameOver += Destroy;
+		
+		GenerateDebugPanel();
+	}
+	
+	public void UpdateExpBar(float ratio)
+	{
+		_expBar.SetDeferred(Range.PropertyName.Value, ratio * 100f);
+	}
+	
+	public void UpdateHpBar(float ratio)
+	{
+		_hpBar.SetDeferred(Range.PropertyName.Value, ratio * 100f);
+	}
+	
+	public void UpdateShieldBar(float ratio)
+	{
+		_shieldBar.SetDeferred(Range.PropertyName.Value, ratio * 100f);
+	}
 
-    public override void OnDestroyUi()
-    {
-        
-    }
-    
-    public override void _Input(InputEvent @event)
-    {
-        if (@event.IsActionPressed("Esc"))
-        {
-            if (UiManager.Get_PausedMenu_Instance().Length <= 0)
-            {
-                return;
-            }
+	public override void OnDestroyUi()
+	{
+		
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("Esc"))
+		{
+			if (UiManager.Get_PausedMenu_Instance().Length <= 0)
+			{
+				return;
+			}
 
-            if (UiManager.Get_PausedMenu_Instance()[0].Visible)
-            {
-                UiManager.Hide_PausedMenu();
-            }
-            else
-            {
-                UiManager.Get_PausedMenu_Instance()[0].ShowUi();
-            }
-        }
-        if (@event.IsActionPressed("OpenConsole"))
-        {
-            _isDebugPanelOpened = !_isDebugPanelOpened;
+			if (UiManager.Get_PausedMenu_Instance()[0].Visible)
+			{
+				UiManager.Hide_PausedMenu();
+			}
+			else
+			{
+				UiManager.Get_PausedMenu_Instance()[0].ShowUi();
+			}
+		}
+		if (@event.IsActionPressed("OpenConsole"))
+		{
+			_isDebugPanelOpened = !_isDebugPanelOpened;
 
-            if (_isDebugPanelOpened)
-            {
-                Global.StopGame();
-                GDebugPanel.Show(_debugPanel); return;
-            }
-            Global.ResumeGame();
-            GDebugPanel.Hide();
-        }
-    }
+			if (_isDebugPanelOpened)
+			{
+				Global.StopGame();
+				GDebugPanel.Show(_debugPanel); return;
+			}
+			Global.ResumeGame();
+			GDebugPanel.Hide();
+		}
+	}
 
-    private void GenerateDebugPanel()
-    {
-        // Player
-        GDebugPanel.AddSection("Player", new PlayerCommands());
+	private void GenerateDebugPanel()
+	{
+		// Player
+		GDebugPanel.AddSection("Player", new PlayerCommands());
 
-        
-        // World
-        var worldCommand = new WorldCommands();
-        var worldSection = GDebugPanel.AddSection("World", worldCommand);
+		
+		// World
+		var worldCommand = new WorldCommands();
+		var worldSection = GDebugPanel.AddSection("World", worldCommand);
 
-        var worldColor = WorldCommands.WorldColors.Red;
-        
-        worldSection.AddEnum("ColorType", val =>
-        {
-            worldColor = val;
+		var worldColor = WorldCommands.WorldColors.Red;
+		
+		worldSection.AddEnum("ColorType", val =>
+		{
+			worldColor = val;
 
-            switch (worldColor)
-            {
-                case WorldCommands.WorldColors.Red:
-                    Global.SetWorldColor(Constants.Colors.Red);
-                    break;
-                case WorldCommands.WorldColors.Blue:
-                    Global.SetWorldColor(Constants.Colors.Blue);
-                    break;
-            }
-        }, () => worldColor);
-        
-    }
+			switch (worldColor)
+			{
+				case WorldCommands.WorldColors.Red:
+					Global.SetWorldColor(Constants.Colors.Red);
+					break;
+				case WorldCommands.WorldColors.Blue:
+					Global.SetWorldColor(Constants.Colors.Blue);
+					break;
+			}
+		}, () => worldColor);
+		
+	}
 }
