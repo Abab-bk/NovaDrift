e51c4df749d99038d5a22f7c94df45e3a8a08725
Hey R You Are <55276676+Abab-bk@users.noreply.github.com>
2024-05-25 21:19:59 +0800

    fix detached head.

--- Src/Scripts/Prefabs/Weapons/Pulse.cs
+++ Src/Scripts/Prefabs/Weapons/Pulse.cs
@@ -1,27 +1,53 @@
 using Godot;
-using System;
+using NovaDrift.Scripts.Prefabs.Actors;
+using NovaDrift.Scripts.Prefabs.Actors.Mobs;
+using NovaDrift.Scripts.Vfx;
 
 namespace NovaDrift.Scripts.Prefabs.Weapons;
 
 public partial class Pulse : BaseShooter
 {
+    // TODO: 需要应用上各种Modifier，包括其他武器
+    private Timer _shootTimer;
+    
     public override void SetShootCd(float value)
     {
-        throw new NotImplementedException();
+        _shootTimer.WaitTime = value;
     }
 
     public override void Init()
     {
-        throw new NotImplementedException();
+        GD.Print("Init");
+        _shootTimer = new Timer
+        {
+            WaitTime = Actor.Stats.ShootSpeed.Value,
+            OneShot = true
+        };
+        AddChild(_shootTimer);
     }
 
     public override void Shoot()
     {
-        throw new NotImplementedException();
+        if (!_shootTimer.IsStopped()) return;
+        _shootTimer.Start();
+        var circleBlast = GD.Load<PackedScene>("res://Scenes/Vfx/CircleBlast.tscn").Instantiate<CircleBlast>();
+        circleBlast.IsPlayer = IsPlayer;
+        circleBlast.OnSomeThingHit += node2D =>
+        {
+            if (node2D is not Actor actor) return;
+            if (Actor.IsPlayer && actor is MobBase)
+            {
+                actor.TakeDamageWithoutKnockBack(Actor.Stats.Damage.Value);
+            }
+            else if (!Actor.IsPlayer && actor is Player)
+            {
+                actor.TakeDamageWithoutKnockBack(Actor.Stats.Damage.Value);
+            }
+        };
+        AddChild(circleBlast);
     }
 
     public override void Destroy()
     {
-        throw new NotImplementedException();
     }
 }
