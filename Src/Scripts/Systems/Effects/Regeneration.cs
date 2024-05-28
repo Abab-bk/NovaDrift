c6568a3f2fd720f1826f3249f10b89a3aa18e15a
Hey R You Are <55276676+Abab-bk@users.noreply.github.com>
2024-05-28 21:27:16 +0800

    添加新Effect、优化文案

--- Src/Scripts/Systems/Effects/Regeneration.cs
+++ Src/Scripts/Systems/Effects/Regeneration.cs
@@ -1,11 +1,47 @@
-﻿namespace NovaDrift.Scripts.Systems.Effects;
+﻿using Godot;
+using NovaDrift.addons.AcidStats;
+
+namespace NovaDrift.Scripts.Systems.Effects;
 
 public class Regeneration : Effect
 {
+    private Timer _timer;
+    
     public override void OnCreate()
     {
+        _timer = new Timer
+        {
+            WaitTime = 1.0f,
+            OneShot = true
+        };
+        Target.AddChild(_timer);
+
+        _timer.Timeout += () =>
+        {
+            AddModifierToTarget(
+                new StatModifier(
+                Target.Stats.Health.BaseValue / Target.Stats.Health.MaxValue.Value, StatModType.PercentAdd, _timer),
+                Target.Stats.Regeneration
+            );
+            _timer.WaitTime = Mathf.Max(0.1, _timer.WaitTime - 0.1);
+            _timer.Start();
+        };
+
+        Target.Stats.Health.ValueToMax += f =>
+        {
+            Target.Stats.Regeneration.RemoveAllModifiersFromSource(_timer);
+        };
+
         base.OnCreate();
         AddModifierToTarget(
             DataBuilder.BuildPercentAddModifier(Values[0]), Target.Stats.Regeneration);
+        
+        _timer.Start();
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        _timer.QueueFree();
     }
 }
